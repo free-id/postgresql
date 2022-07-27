db679273736381dc1ef86e924cb572795e8eaf7c
Vitaly Kuzyaev <vitkuz573@gmail.com>
2022-07-27 12:53:06 +0500

    refactor(main): create parser

--- src/Parser.php
+++ src/Parser.php
@@ -2,19 +2,44 @@
 
 declare(strict_types=1);
 
-namespace VendorName\Skeleton;
+namespace FreeId\Postgresql;
 
+use FreeId\Core\Concerns\Database;
+use FreeId\Contracts\SqlDatabase as SqlDatabaseContract;
 use FreeId\Core\Parser as BaseParser;
 
-class Parser extends BaseParser
+class Parser extends BaseParser implements SqlDatabaseContract
 {
-    public function __construct() {
-        // Constructor code ...
-    }
+    use Database;
 
+    public function __construct(
+        string $host,
+        string $port,
+        string $db,
+        string $table,
+        array  $credentials,
+        string $column = 'id',
+        string $charset = 'utf8',
+        int    $start_id = 1,
+    ) {
+        parent::__construct([], $start_id);
+        $this->host = $host;
+        $this->port = $port;
+        $this->db = $db;
+        $this->table = $table;
+        $this->credentials = $credentials;
+        $this->column = $column;
+        $this->charset = $charset;
+    }
+    
     public function find(): int
     {
-        // Parser code ...
+        $this->data = $this->getData(
+            'pgsql:host=' . $this->host . ';port=' . $this->port . ';dbname=' . $this->db . ';options=\'--client_encoding=' . $this->charset . '\'',
+            $this->credentials,
+            $this->table,
+            $this->column,
+        );
 
         return $this->enumerate();
     }
